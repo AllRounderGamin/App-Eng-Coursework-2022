8c97b7e3f6185293c734f2d3f0780cac73686eca
AllRounderGamin <78315461+AllRounderGamin@users.noreply.github.com>
2022-02-12 04:01:28 +0000

    Added Product Pages

--- Server.js
+++ Server.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.use(express.static("Webpages", {extensions: ['html']}));
 
+function redirects(req, res) {
+    res.redirect("landing.html")
+}
+
+app.get("/", redirects)
+
 
 console.log("Server Listening!");
 app.listen(8080);
--- Webpages/Scripts.js
+++ Webpages/Scripts.js
@@ -40,3 +40,36 @@ async function createCartContent(ID) {
     const finalPrice = document.querySelector("#totalPrice");
     finalPrice.textContent = "Total: £" + totalAmount.toFixed(2);
 }
+
+async function fillProductsPage(buffer) {
+    const productData = await fetch("./testProduct.json");
+    const products = await productData.json();
+    const productPage = document.getElementById("productPage");
+    for (let i = 0; i < 12; i++) {
+        const productDiv = document.createElement("div");
+        const itemLink = document.createElement("a");
+        const itemImage = document.createElement("img");
+        const productName = document.createElement("h3");
+
+        console.log(products[i + buffer])
+        console.log(products);
+        if (products[i + buffer]) {
+            productName.textContent = products[i + buffer].name;
+            itemImage.alt = productName.textContent;
+            itemImage.src = products[i + buffer].imageUrl;
+            itemLink.href = "product?name=" + encodeURIComponent(productName.textContent);
+
+            itemImage.classList.add("product");
+
+            itemLink.appendChild(itemImage)
+            productDiv.appendChild(itemLink);
+            productDiv.appendChild(productName);
+            productPage.appendChild(productDiv);
+        }
+    }
+}
+
+async function fillProductInfo(params) {
+    const testHeading = document.getElementById("placeholder")
+    testHeading.textContent = params.get("name");
+}
