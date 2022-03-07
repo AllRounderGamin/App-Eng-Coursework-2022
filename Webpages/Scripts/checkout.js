69426ae50f731b89e2411836045e3e83b6644cd7
AllRounderGamin <78315461+AllRounderGamin@users.noreply.github.com>
2022-03-07 04:07:10 +0000

    JS Refactor

--- Webpages/Scripts/checkout.js
+++ Webpages/Scripts/checkout.js
@@ -1,7 +1,6 @@
 import {addToList} from "./fetchLists.js";
 
 export async function checkout() {
-    // validate info https://smallbusiness.chron.com/identify-credit-card-account-number-61050.html
     const cart = JSON.parse(localStorage.getItem("cart"));
     const image = cart[0].src;
     const date = new Date();
@@ -15,7 +14,7 @@ export function verifyCardNum(e) {
     const text = document.querySelector("#CardIdentity");
     if (e.target.value === "") {
         text.textContent = "Enter Card Number";
-        return
+        return;
     }
     if (!Luhn(e.target.value)) {
         text.textContent = "Unknown Card Type";
@@ -25,7 +24,7 @@ export function verifyCardNum(e) {
 }
 
 function Luhn(num) {
-    let reversed = num.toString().split('').reverse()
+    let reversed = num.toString().split('').reverse();
     const checkDigit = parseInt(reversed[0]);
     let total = 0;
     reversed[0] = 0;
@@ -40,7 +39,7 @@ function Luhn(num) {
             total += parseInt(reversed[i]);
         }
     }
-    let checkDigitTest = (10 - total % 10)
+    let checkDigitTest = (10 - total % 10);
     if (checkDigitTest === 10) {
         checkDigitTest = 0;
     }
@@ -63,15 +62,5 @@ function cardType(num) {
                     return "Carte Blanche";
             }
     }
-    return "Unknown Card Type"
+    return "Unknown Card Type";
 }
-
-// Luhn's Algorithm + Card type verify
-// Have a hidden text below card input that becomes visible and changes depending on results
-// https://www.dignited.com/47628/debit-credit-card-number-cvv-and-expiry-date-explained/#:~:text=The%20last%20%2816th%29%20digit%20in%20your%20debit%20card,and%20ensures%20that%20the%20numbers%20weren%E2%80%99t%20entered%20incorrectly.
-// do you validate card by validating check digit over whole number? (10 - total mod 10) mod 10
-// https://simplycalc.com/luhn-validate.php
-
-// DONE IT
-// Take last digit and convert it to 0, then double each second digit starting from 0 (right to left), then after
-// 10 mod calc the result should equal the 16th digit
