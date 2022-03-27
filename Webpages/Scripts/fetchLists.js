87bfd1a0bd057bd6391001d766d7ff913fa79df5
AllRounderGamin <78315461+AllRounderGamin@users.noreply.github.com>
2022-03-27 18:29:58 +0100

    Removed Duplicate ID from Template Transition

--- Webpages/Scripts/fetchLists.js
+++ Webpages/Scripts/fetchLists.js
@@ -1,7 +1,7 @@
 import {loadFromUrl} from "./pageLoading.js";
 
 export async function changeList(e) {
-    const test = document.querySelector("#ListArea")
+    const test = document.querySelector(".ListArea")
     while (test.hasChildNodes()) {
         test.removeChild(test.firstChild);
     }
@@ -68,7 +68,7 @@ export function loadList(listName) {
     if (!items) {
         return;
     }
-    const list = document.querySelector("#ListArea");
+    const list = document.querySelector(".ListArea");
     for (let item of items) {
         const cumulativePrice = item.singlePrice * item.amount;
         const itemName = item.name + " x" + item.amount.toString();
@@ -87,7 +87,7 @@ export function loadList(listName) {
         position++;
     }
     if (listName !== "Wishlist") {
-        const finalPrice = document.querySelector("#totalPrice");
+        const finalPrice = document.querySelector(".totalPrice");
         finalPrice.textContent = "Total: £" + totalAmount.toFixed(2);
     }
 }
@@ -95,7 +95,7 @@ export function loadList(listName) {
 async function loadRecent() {
     let position = 0;
     const items = JSON.parse(localStorage.getItem("recentPurchases")).reverse();
-    const list = document.querySelector("#ListArea");
+    const list = document.querySelector(".ListArea");
     if (!items) {
         return;
     }
@@ -116,7 +116,7 @@ export function loadPurchase(params) {
         return;
     }
     items = items[num].cart;
-    const list = document.querySelector("#ListArea");
+    const list = document.querySelector(".ListArea");
     for (let item of items) {
         const cumulativePrice = (item.singlePrice * item.amount);
         const itemName = item.name + " x" + item.amount.toString();
